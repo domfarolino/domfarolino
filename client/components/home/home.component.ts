5a9e1c729596e30bec3b73e79400a647e5a2ba03
Dom Farolino <domfarolino@gmail.com>
2016-04-03 11:38:12 -0400

    Finished basic home component styles for button and md-card

--- client/components/home/home.component.ts
+++ client/components/home/home.component.ts
@@ -1,12 +1,12 @@
 import { Component } from 'angular2/core';
 import { MdButton } from '@angular2-material/button';
-import { MdCard } from '@angular2-material/card';
+import { MD_CARD_DIRECTIVES } from '@angular2-material/card';
 
 @Component({
   selector: 'home',
   templateUrl: 'client/components/home/home.component.html',
   styleUrls: ['client/components/home/home.component.css'],
-  directives: [MdButton, MdCard]
+  directives: [MdButton, MD_CARD_DIRECTIVES]
 })
 
 export class HomeComponent{
