a25104f5b682ee46f71cf822ecbcfe980ab1c4dd
Dom Farolino <domfarolino@gmail.com>
2016-04-03 20:26:15 -0400

    Added service worker and caching functionality

---
+++ sw.js
@@ -0,0 +1,53 @@
+var staticCacheName = 'dom-static-v1';
+//dfefwaf
+self.addEventListener('install', function(event) {
+  console.log("Installing service worker");
+  // event.waitUntil(
+  //   caches.open(staticCacheName).then(function(cache) {
+  //     return cache.addAll([
+  //       '/'
+  //     ]);
+  //   })
+  // );
+});
+
+self.addEventListener('activate', function(event) {
+  console.log("Activated service worker");
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(cacheName) {
+          return cacheName.startsWith('dom-') &&
+                 cacheName != staticCacheName;
+        }).map(function(cacheName) {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
+
+// Cache then network strategy
+// https://jakearchibald.com/2014/offline-cookbook/#cache-then-network
+self.addEventListener('fetch', function(event) {
+  if (event.request.method != 'GET') {
+     event.respondWith(fetch(event.request));
+     return;
+  }
+  event.respondWith(
+    caches.open(staticCacheName).then(function(cache) {
+      return cache.match(event.request).then(function(response) {
+        if (response) {
+          console.log("Got from cache");
+          return response;
+        }
+        
+        return fetch(event.request).then(function(response) {
+          console.log("Got from server");
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      })
+    })
+  );
+});
