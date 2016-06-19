3fd7bdb7c8c6a7d43fc9e62b9b18578b118239a1
Dom Farolino <domfarolino@gmail.com>
2016-06-19 13:36:53 -0400

    feat(): tmp resume page, upgraded ng components to rc.2, fullscreen sidenav layout

--- src/app/resume/resume.component.ts
+++ src/app/resume/resume.component.ts
@@ -1,16 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { ROUTER_DIRECTIVES, Router } from '@angular/router';
+import { MdButton } from '@angular2-material/button';
+import { MD_CARD_DIRECTIVES } from '@angular2-material/card';
 
 @Component({
   moduleId: module.id,
   selector: 'app-resume',
   templateUrl: 'resume.component.html',
-  styleUrls: ['resume.component.css']
+  styleUrls: ['resume.component.css'],
+  directives: [ROUTER_DIRECTIVES, MdButton, MD_CARD_DIRECTIVES]
 })
 export class ResumeComponent implements OnInit {
 
-  constructor() {}
+  constructor(private _router: Router) {}
 
   ngOnInit() {
+  }
+  
+  goHome() {
+    this._router.navigate(['/']);
   }
 
 }
