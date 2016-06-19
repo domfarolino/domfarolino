4d126861a978ed9dea58486b71f22021b2a55497
Dom Farolino <domfarolino@gmail.com>
2016-06-19 13:55:24 -0400

    test(resumecomponent): corrected resumecomponent spec (DI)

--- src/app/resume/resume.component.spec.ts
+++ src/app/resume/resume.component.spec.ts
@@ -7,14 +7,16 @@ import {
   beforeEach, beforeEachProviders,
   describe, xdescribe,
   expect, it, xit,
-  async, inject
+  async, inject, injectAsync
 } from '@angular/core/testing';
 
 import { ResumeComponent } from './resume.component';
+import { Router } from '@angular/router';
 
 describe('Component: Resume', () => {
-  it('should create an instance', () => {
-    let component = new ResumeComponent();
+  it('should create an instance', injectAsync(
+        [Router], (_router: Router) => {
+    let component = new ResumeComponent(_router);
     expect(component).toBeTruthy();
-  });
+  }));
 });
